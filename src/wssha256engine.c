f40c02327b5e52960e8b517318c03fc9b7519877
Brett <brett.r.nicholas@gmail.com>
2017-05-19 11:58:43 -0400

    first attempt at working engine integration

--- src/wssha256engine.c
+++ src/wssha256engine.c
@@ -1,7 +1,10 @@
 #include <openssl/engine.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 #include <stdlib.h>
+
+#include "wssha.h"
 
 // Turn off this annoying warning that we don't care about 
 #pragma GCC diagnostic ignored "-Wsizeof-pointer-memaccess"
@@ -9,11 +12,11 @@
 #define FAIL 0
 #define SUCCESS 1
 #define DIGEST_SIZE_BYTES 32
+#define MESSAGE_SIZE_BYTES 256
 
 static const char *engine_id = "wssha256";
 static const char *engine_name = "A test engine for the ws sha256 hardware encryption module, on the Xilinx ZYNQ7000";
 static int wssha256_digest_ids[] = {NID_sha256,0};
-
 
 static int wssha256engine_sha256_init(EVP_MD_CTX *ctx);
 static int wssha256engine_sha256_update(EVP_MD_CTX *ctx, const void *data, size_t count);
@@ -21,9 +24,30 @@ static int wssha256engine_sha256_final(EVP_MD_CTX *ctx, unsigned char *md);
 int wssha256engine_sha256_copy(EVP_MD_CTX *to, const EVP_MD_CTX *from);
 static int wssha256engine_sha256_cleanup(EVP_MD_CTX *ctx);
 
+static const uint32_t digest_len = DIGEST_SIZE_BYTES;
 
 // Create our own message digest hash SHA256 declaration matching that of the generic 
 // SHA256 message digest structure (struct env_md_st) defined in openssl/include/evp.h
+//struct env_md_st {
+//    int type;
+//    int pkey_type;
+//    int md_size;
+//    unsigned long flags;
+//    int (*init) (EVP_MD_CTX *ctx);
+//    int (*update) (EVP_MD_CTX *ctx, const void *data, size_t count);
+//    int (*final) (EVP_MD_CTX *ctx, unsigned char *md);
+//    int (*copy) (EVP_MD_CTX *to, const EVP_MD_CTX *from);
+//    int (*cleanup) (EVP_MD_CTX *ctx);
+//    int (*sign) (int type, const unsigned char *m, unsigned int m_length,
+//                 unsigned char *sigret, unsigned int *siglen, void *key);
+//    int (*verify) (int type, const unsigned char *m, unsigned int m_length,
+//                   const unsigned char *sigbuf, unsigned int siglen,
+//                   void *key);
+//    int required_pkey_type[5];  /* EVP_PKEY_xxx */
+//    int block_size;
+//    int ctx_size;               /* how big does the ctx->md_data need to be */
+//    int (*md_ctrl) (EVP_MD_CTX *ctx, int cmd, int p1, void *p2); /* control function */
+//} 
 static EVP_MD wssha256engine_sha256_method = 
 {
     NID_sha256, // openSSL algorithm numerical ID
@@ -50,6 +74,11 @@ static EVP_MD wssha256engine_sha256_method =
 static int wssha256engine_sha256_init(EVP_MD_CTX *ctx)
 {
   ctx->update = &wssha256engine_sha256_update;
+  if (sha256_init() < 0)
+  {
+    fprintf(stderr,"ERROR: SHA256 algorithm context could not be initialized\n");
+    return FAIL;
+  }
   printf("SHA256 algorithm context initialized\n");
   return SUCCESS;
 }
@@ -63,8 +92,15 @@ static int wssha256engine_sha256_update(EVP_MD_CTX *ctx, const void *data, size_
   printf("SHA256 update\n");
   unsigned char *digest = (unsigned char*)malloc(sizeof(unsigned char) * DIGEST_SIZE_BYTES);
   // TODO change below to actual implementation
-  memset(digest, 2, DIGEST_SIZE_BYTES);
+  //memset(digest, 2, DIGEST_SIZE_BYTES); // set digest to all 2's
   //count = 32; 
+  int status = sha256((uint8_t*)data, MESSAGE_SIZE_BYTES, (uint8_t*)digest, (uint32_t*)&digest_len);
+  if (status < 0)
+  {
+    fprintf(stderr,"ERROR: SHA256 algorithm failed\n");
+    return FAIL;
+  }
+
   ctx->md_data = digest;
   return SUCCESS;
 }
@@ -72,6 +108,7 @@ static int wssha256engine_sha256_update(EVP_MD_CTX *ctx, const void *data, size_
 
 /*
  * Digest final update function 
+ * Copies ctx->md_data into output buffer (TODO this is redundant)
  */
 static int wssha256engine_sha256_final(EVP_MD_CTX *ctx, unsigned char *md)
 {
